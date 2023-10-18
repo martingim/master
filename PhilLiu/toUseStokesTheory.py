ba848224b39cd79c4c875e282c0b4e3264a464b3
martingim <martingimse@gmail.com>
2023-10-18 14:24:30 +0200

    Python version

--- PhilLiu/toUseStokesTheory.py
+++ PhilLiu/toUseStokesTheory.py
@@ -1,58 +1,60 @@
-% % Stokes theory, 
-% % 1. input region
-% clc,clear
+from StokesDispSolver import StokesDispSolver
+from StokesU import stokes_u
+from StokesEta import StokesEta
 
-h0 = 10; 
-% For the first mode, a is first harmonic first order amplitude
-modeNo = 1; 
-a = 1.6; % either a or H is given
-% H = 3.2; % either a or H is given
-T = 5;
-theta0 = 0:0.01:2*pi;
-%
-Result =StokesDispSolver('h', h0, 'T', T, 'a', a,'mode', modeNo)
-if  modeNo ==1
-    a = Result.a;
-    H = Result.H;
-elseif modeNo ==2
-    a = Result.aw;
-    H = Result.Hw;
-end
+import numpy as np
+import matplotlib.pyplot as plt
 
-% Below is to call the free surface function StokesEta, make your own edit to select the preferred output. 
-[eta, eta1, eta2, eta3,eta4,eta5] = StokesEta(Result.k, h, Result.a, theta); % free surface
-% To export eta only, use below, or modify the output arguments.
-% [eta, ~,~,~,~,~] = StokesEta(Result.k, h, Result.a, theta);
+# Input region
+h0 = 10
+modeNo = 1
+a = 1.6
+T = 5
+theta0 = np.arange(0, 2 * np.pi, 0.01)
 
-plot(theta0,eta)
-c = Result.L/T; % phase speed
+# Call StokesDispSolver
+Result = StokesDispSolver('h', h0, 'T', T, 'a', a, 'mode', modeNo)
 
-% velocity and potential function
-% example below for results at 11 elevations
-phi = zeros(1,11); u1 = zeros(1,11); u2 = zeros(1,11); u3 = zeros(1,11); u4 = zeros(1,11); u5 = zeros(1,11);
-w1 = zeros(1,11); w2 = zeros(1,11); w3 = zeros(1,11); w4 = zeros(1,11); w5 = zeros(1,11);
+if modeNo == 1:
+    a = Result.a
+    H = Result.H
+elif modeNo == 2:
+    a = Result.aw
+    H = Result.Hw
 
-for i = 1:length(theta)    
-% z = [-10:0.5:-1.5, eta(i)];%linspace(-h, eta(i), 10);
-z = [linspace(-h, eta(i), 10), -2.5];% In this case select 11 elevations for calculation
+# Call StokesEta function
+theta = theta0  # Assuming theta0 is the same as theta in the MATLAB code
+eta, eta1, eta2, eta3, eta4, eta5 = StokesEta(Result.k, h0, Result.a, theta)
 
-% To export phi, u, w only, use below, or modify the output arguments.
-% [phi, u,~,~,~,~,~, w,~,~,~,~,~] = StokesU(k, h, a, theta, z)
+# Plot the result
+plt.plot(theta0, eta)
+c = Result.L / T  # phase speed
 
-[phi(i,:), u, u1(i,:),u2(i,:),u3(i,:),u4(i,:),u5(i,:), ...
-    w,w1(i,:),w2(i,:),w3(i,:),w4(i,:),w5(i,:)] = StokesU(Result.k, h, Result.a, theta(i), z);
-    
-end
+# Velocity and potential function
+phi = np.zeros(11)
+u1 = np.zeros(11)
+u2 = np.zeros(11)
+u3 = np.zeros(11)
+u4 = np.zeros(11)
+u5 = np.zeros(11)
+w1 = np.zeros(11)
+w2 = np.zeros(11)
+w3 = np.zeros(11)
+w4 = np.zeros(11)
+w5 = np.zeros(11)
 
-Result.u = u;
-Result.w = w;
+for i in range(len(theta)):
+    z = np.concatenate([np.linspace(-h, eta[i], 10), [-2.5]])
+    phi[i], u, u1[i], u2[i], u3[i], u4[i], u5[i], w, w1[i], w2[i], w3[i], w4[i], w5[i] = StokesU(Result.k, h0, Result.a, theta[i], z)
 
-% etaFen = FentonEta(Result.k, h0, a, theta0)
+Result.u = u
+Result.w = w
 
-% % For the second mode, a is combined first harmonic amplitude
-% modeNo = 2; 
-% Result2 =StokesDispSolver('h', h0, 'T', T0, 'a', a,'mode', 2);
+# To-Do: Implement FentonEta function if needed
+# etaFen = FentonEta(Result.k, h0, a, theta0)
 
-% Then we can use StokesEta and StokesU functions
+# For the second mode, a is combined first harmonic amplitude
+# modeNo = 2
+# Result2 = StokesDispSolver('h', h0, 'T', T0, 'a', a, 'mode', 2)
 
-
+# Then you can use StokesEta and StokesU functions for Result2

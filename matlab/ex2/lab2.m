4306333a930fad67cdb18b740469cb47a509f720
martingim <martingimse@gmail.com>
2023-11-03 13:44:05 +0100

    added matlab files from experiment

---
+++ matlab/ex2/lab2.m
@@ -0,0 +1,76 @@
+%Setup
+image_names
+coord_config
+h = 0.33; %heightof the water surface at rest
+
+close all
+%% Perform PIV
+n_runs = 15;
+n_waves = 1;
+max_arrows = 50; %max number of arrows in one dim in the quiver plots
+%height = 0.33;
+
+%%Perform PIV
+for run_number=11:n_runs
+    for wave=1:n_waves
+        perform_PIV(run_number, wave);
+        disp('PIV performed');
+    end
+end
+
+%% Plot velocities
+run_number =8;
+pair_number = 1;
+
+im1 = imread(image_name(run_number, pair_number*2-1));
+im2 = imread(image_name(run_number, pair_number*2));
+mask1_name = image_name(run_number, pair_number*2-1) + ".mask.mat";
+mask2_name = image_name(run_number, pair_number*2) + ".mask.mat";
+
+load(mask1_name);
+load(mask2_name);
+%surface 
+[idx1,eta1] = max(mask1);
+[idx2,eta2] = max(mask2);
+
+[etax,etay] = tformfwd(tform(ceil(run_number/5)),1:size(im1,2),(eta1+eta2)/2);
+
+load('velocities.mat')
+load('params.mat')
+run_number = 8;
+alpha = containers.Map('KeyType', 'double', 'ValueType', 'any');
+crest_mask = containers.Map('KeyType', 'double', 'ValueType', 'any');
+y_scaled = containers.Map('KeyType', 'double', 'ValueType', 'any');
+for wave = 1:3
+    [y_scaled(wave), u_crest_scaled, alpha(wave), crest_mask(wave)] = plot_velocities(run_number, wave, 50);
+    %close all
+end
+
+mask = crest_mask(1)&crest_mask(2)&crest_mask(3);
+
+y1 = y_scaled(1);
+y2 = y_scaled(2);
+y3 = y_scaled(3);
+a1 = alpha(1);
+a2 = alpha(2);
+a3 = alpha(3);
+
+[tmp, i] = min([size(y1, 1) size(y2, 1) size(y3, 1)]);
+y = y_scaled(i);
+
+alpha_mean = (a1 + a2 + a3)/3;
+
+plot(alpha_mean(mask), y(:,1), 'x')
+hold on
+
+
+plot(a1(crest_mask(1)), y1(:,1), 'x')
+plot(a2(crest_mask(2)), y2(:,1), 'x')
+plot(a3(crest_mask(3)), y3(:,1), 'x')
+p = params(run_number);
+k = p('k');
+a = p('a');
+plot(exp(k*y1(:,1)*h), y1(:,1))
+legend('mean', '1', '2', '3', 'analytical')
+
+
---
+++ matlab/ex2/surface_height.m
@@ -0,0 +1,116 @@
+function [a] = surface_height(run_number)
+image_names
+height_config
+folder = day_folder(run_number);
+f = frequency(run_number); %The frequency of the run
+[omega,T,K,LAMBDA,CP,CG] = wparam(f, 0.33); %calculate the wavenumber and phase velocity
+sensor_distance = 0.4; %the dstance between the sensors in meters(should maybe change this to a vector)
+%start and stop frame for measuring the elevation of the surface
+surface_frame_start = surface_start_stop(run_number, 1);
+surface_frame_end = surface_start_stop(run_number, 2);
+surface_frame_end = surface_frame_end + rem(surface_frame_end-surface_frame_start+1, 2);%to simplify the fft
+
+sensor_samplerate = 100; %Hz
+min_separation = 1/f*sensor_samplerate*0.8; %minimum separation when finding the crests and troughs
+%read sensor data from file
+sensordata = table2array(readtable(folder + sprintf("/run%d.csv", run_number)));
+
+%remove zeros
+sensordata = sensordata(:,5:8);
+sensordata(sensordata<350|sensordata>850) = NaN; %replace the outliers with NaN
+
+%convert the data from the sensors to measured surface elevation in meters
+measured_surface = [sensor1(sensordata(:,1)), sensor2(sensordata(:,2)) sensor3(sensordata(:,3)) sensor4(sensordata(:,4))];
+
+%subtract the mean from the surface to get zero mean
+surface_mean = mean(measured_surface, 'omitnan');
+measured_surface = measured_surface -surface_mean;
+
+%plot the sensor data from all four sensors with the same waves on top of
+%each other
+sensor_offset = round(sensor_samplerate*sensor_distance/CP)-2; %sensor offset in samples to plot the same waves on top of each other
+figure;
+hold on 
+plot(measured_surface(:,1))
+plot(measured_surface(1*sensor_offset:end,2))
+plot(measured_surface(2*sensor_offset:end,3))
+plot(measured_surface(3*sensor_offset:end,4))
+legend('sensor1', 'sensor2', 'sensor3', 'sensor4')
+
+%Find the crests on troughs from the first sensor and plot with the
+%measured data
+time = (1:size(measured_surface, 1))/100;
+figure;
+hold on
+surf = measured_surface(:,1);
+plot(time, measured_surface(:,1))
+LMax = islocalmax(measured_surface(:,1), 'MinSeparation', min_separation);
+LMin = islocalmin(measured_surface(:,1), 'MinSeparation', min_separation);
+plot(time(LMin), surf(LMin), 'x')
+plot(time(LMax), surf(LMax), 'x')
+
+
+%%
+%find the crests and troughs of the measured surface to find the mean
+%amplitude of the waves. %%maybe change this later to only look at one wave
+surf = measured_surface(surface_frame_start:surface_frame_end,:); %change this to use the sensor_offset
+mean_amplitude = zeros(size(surf, 2),1);
+for i=1:size(surf, 2)
+    LMax = islocalmax(surf(:,i), 'MinSeparation', min_separation);
+    LMin = islocalmin(surf(:,i), 'MinSeparation', min_separation);
+    mean_amplitude(i) = (mean(surf(LMax,i))-mean(surf(LMin, i)))/2;
+end
+
+a = mean(mean_amplitude);
+
+
+%% plot the measured surface with theoretical linear and non linear
+t = 0:0.001:2;
+eta = a*cos(-omega*t); %the linear surface
+eta_non_linear = a*cos(-omega*t) +  0.5*a^2*K*cos(2*(-omega*t)) + 3/8*K^2*a^3*cos(3*(-omega*t)); %the non-linear surface
+
+%find the first crest to start plotting from
+crests = islocalmax(surf(:,1), 'MinSeparation', 10);
+crest_found = -1;
+while crest_found<0
+    [m, start_idx] = max(crests); %the index to start plotting the measured surface from
+    
+        
+    if surf(start_idx)<0.9*a
+        crests(start_idx) = 0;
+    else
+        crest_found=1;
+    end
+end
+
+%plot the surfaces
+figure
+hold on
+plot(t, eta)
+plot(t, eta_non_linear)
+t_measured = 0:0.01:2;
+plot(t_measured, surf(start_idx:start_idx+size(t_measured, 2)-1,1)')
+legend('linear', 'non-linear', 'measured')
+
+
+%% FFT of the measured surface
+X = fillmissing(surf(:,2),'linear');
+Y = fft(X); 
+
+L = size(surf, 1);
+P2 = abs(Y/L);
+P1 = P2(1:L/2+1);
+P1(2:end-1) = 2*P1(2:end-1);
+Fs = 100;
+frequencies = Fs*(0:(L/2))/L;
+
+figure;
+hold on
+plot(frequencies, P1)
+plot(f, 0, 'x')
+plot(2*f, 0, 'x')
+plot(3*f, 0, 'x')
+legend('fft of the measured surface', 'f', '2f', '3f')
+xlabel('frequency[Hz]')
+xlim([0 f*3.2])
+end

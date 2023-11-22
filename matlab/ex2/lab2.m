9dc112d8aac2c0779d09369bddc51ae2855b448a
martingim <martingimse@gmail.com>
2023-11-22 13:50:46 +0100

    moving stuff to functions

--- matlab/ex2/lab2.m
+++ matlab/ex2/lab2.m
@@ -6,16 +6,16 @@ cd(current_path)
 image_names
 coord_config
 h = 0.33; %heightof the water surface at rest
-
+n_runs = 15;%number of runs in the wave tank
+n_waves = 3;%number of wave image pairs to use per run
 close all
 
 %% Perform PIV
 force_PIV = false; %true: Force recalculating the PIV or 
 %                   false:just PIV on the ones that 
 %                         aren't saved in velocities.mat
-n_runs = 15;
-n_waves = 3;
-%height = 0.33;
+
+
 
 %%Perform PIV
 
@@ -37,83 +37,73 @@ for run_number=1:n_runs
     end
 end
 
-
+%% Surface height and calculate the amplitude of the waves
+for run_number=1:n_runs
+    surface_height(run_number);
+end
+close all
 
 %% Quiver plot
 run_number = 8;
-pair_number = 2;
+pair_number = 1;
 max_arrows = 50; %maximum number of arrows in one dimension in the quiver plot
 quiver_plot(run_number, pair_number, max_arrows)
 
 
 %% plot velocity under crest
-run_number = 8;
-pair_number = 1;
+run_number = 3;
+pair_number = 2;
 plot_velocity_under_crest(run_number, pair_number);
 
 %% Plot Alpha for one wave 
 run_number = 8;
 pair_number = 3;
-plot_alpha(run_number, pair_number)
+create_plot = true;
+plot_alpha(run_number, pair_number, create_plot)
 
-%% Plot alpha for one run
+%% Plot mean alpha for the three chosen waves from one run
+close all
+clear alpa_mean
+run_number = 14;
+create_plot = false;
+for i=1:3
+    if i==1
+        [alpha_mean, alpha_theoretical, y_at_crest, y_below_crest_scaled] = plot_alpha(run_number , i, create_plot);
+    else
+        [alpha, ~, ~, ~] = plot_alpha(run_number, i, create_plot);
+        alpha_mean = alpha + alpha_mean;
+    end
+end
+alpha_mean = alpha_mean/3;
+figure;
+hold on
+plot(alpha_mean,y_at_crest, 'x')
+plot(alpha_theoretical, y_below_crest_scaled)
+title(sprintf('mean alpha for run %d', run_number))
+xlabel('$\alpha$', 'interpreter', 'latex', 'FontSize', 20)
+ylabel('$\frac{y}{h}$', 'interpreter', 'latex', 'FontSize', 20, 'rotation', 0)
 
-%% Plot alpha for all runs
+%% Compare velocity profile to Stokes 5th order
+number_of_runs = 15;
+load params.mat params
 
-
-%% Plot velocities
-run_number =8;
-pair_number = 1;
-
-im1 = imread(image_name(run_number, pair_number*2-1));
-im2 = imread(image_name(run_number, pair_number*2));
-mask1_name = image_name(run_number, pair_number*2-1) + ".mask.mat";
-mask2_name = image_name(run_number, pair_number*2) + ".mask.mat";
-
-load(mask1_name);
-load(mask2_name);
-%surface 
-[idx1,eta1] = max(mask1);
-[idx2,eta2] = max(mask2);
-
-[etax,etay] = tformfwd(tform(ceil(run_number/5)),1:size(im1,2),(eta1+eta2)/2);
-
-load('velocities.mat')
-load('params.mat')
-run_number = 8;
-alpha = containers.Map('KeyType', 'double', 'ValueType', 'any');
-crest_mask = containers.Map('KeyType', 'double', 'ValueType', 'any');
-y_scaled = containers.Map('KeyType', 'double', 'ValueType', 'any');
-for wave = 1:3
-    [y_scaled(wave), u_crest_scaled, alpha(wave), crest_mask(wave)] = plot_velocities(run_number, wave);
-    %close all
+for run_number=1:number_of_runs
+    msg = sprintf('calculating 5th order k for run:%d', run_number);
+    disp(msg)
+    k_5th = find_stokes5th_k(run_number);
+    p = params(run_number);
+    k = p('k');
+    k_str = sprintf("k from second order %f, k from 5th order %f", k, k_5th);
+    disp(k_str)
+    close all
 end
 
-mask = crest_mask(1)&crest_mask(2)&crest_mask(3);
 
-y1 = y_scaled(1);
-y2 = y_scaled(2);
-y3 = y_scaled(3);
-a1 = alpha(1);
-a2 = alpha(2);
-a3 = alpha(3);
-
-[tmp, i] = min([size(y1, 1) size(y2, 1) size(y3, 1)]);
-y = y_scaled(i);
-
-alpha_mean = (a1 + a2 + a3)/3;
-
-plot(alpha_mean(mask), y(:,1), 'x')
-hold on
-
-
-plot(a1(crest_mask(1)), y1(:,1), 'x')
-plot(a2(crest_mask(2)), y2(:,1), 'x')
-plot(a3(crest_mask(3)), y3(:,1), 'x')
-p = params(run_number);
-k = p('k');
-a = p('a');
-plot(exp(k*y1(:,1)*h), y1(:,1))
-legend('mean', '1', '2', '3', 'analytical')
-
+%%
+close all
+for run_number=1:number_of_runs
+    for pair_number=1:n_waves
+        compare_stokes_5th_velocity_profile(run_number, pair_number)
+    end
+end
 

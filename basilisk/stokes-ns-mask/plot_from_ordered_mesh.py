972315be852a8e6b22a19757b3513e9bb41a731b
martingim <martingimse@gmail.com>
2024-02-03 01:00:39 +0100

    read from vtk file

---
+++ basilisk/stokes-ns-mask/plot_from_ordered_mesh.py
@@ -0,0 +1,21 @@
+from meshio import read
+import matplotlib.pyplot as plt
+
+
+filename = "vtk/TIME-000000.vtk"
+
+mesh = read(filename)
+
+
+cell_points = mesh.cells[0]
+
+
+points = mesh.points
+pointData = mesh.point_data
+mask = pointData['f']<2
+X = points[:,0]
+Y = points[:,1]
+U = pointData['u.x']
+V = pointData['u.y']
+plt.quiver(X[mask[:,0]], Y[mask[:,0]], U[mask[:,0]], V[mask[:,0]])
+plt.show()

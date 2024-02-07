b9ad451e8d188b4949cb344bdb7c121fcd1f236c
martingim <martingimse@gmail.com>
2024-02-07 10:48:52 +0100

    interpolates and saves
    uses scipy to interpolate to a regular grid
    and uses scipy to save the interpolated data to a .mat file

--- basilisk/stokes-ns-mask/read_vtu_python.py
+++ basilisk/stokes-ns-mask/read_vtu_python.py
@@ -2,13 +2,14 @@ from meshio import read
 import matplotlib.pyplot as plt
 from numpy import mean
 import numpy as np
+import scipy
    
-filename = "vtu/ascii/ascii_0.vtu"
+filename = "vtu/ascii/TIME-000*_000017/TIME-000*_000017_0.vtu"
 
-mesh = read(filename)
+mesh = read(filename) 
 
 
-cell_points = mesh.cells[0].data
+cell_points = mesh.cells[0].data    
 points = mesh.points
 
 def get_cell_centers():
@@ -26,7 +27,27 @@ V = Ux[0][:,1]
 
 X = get_cell_centers()[:,0]
 Y = get_cell_centers()[:,1]
-
+center_points = np.stack([X, Y], axis=1)
+center_U = np.stack([U,V], axis=1)
 
 plt.quiver(X[mask], Y[mask], U[mask], V[mask])
 plt.show()
+
+Y0 = np.linspace(-0.53, 0.0093, 1000)
+X0 = -0.0755*np.ones_like(Y0)
+interpolation_points = np.stack([X0, Y0], axis=1)
+
+interpolated_U = scipy.interpolate.griddata(center_points, center_U, interpolation_points) 
+
+plt.plot(interpolated_U[:,0], interpolation_points[:,1])
+plt.xlabel("u[m/s]")
+plt.ylabel("y")
+plt.show()
+
+
+matlab_dict  = {}
+matlab_dict['X'] = interpolation_points
+matlab_dict['U'] = interpolated_U
+matlab_filename  = "basilisk_velocity_profile.mat"
+
+scipy.io.savemat(matlab_filename, matlab_dict)

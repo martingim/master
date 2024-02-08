eed6be45b5cc3ff3a8d8c8e5ee268af22476a4ee
martingim <martingimse@gmail.com>
2024-02-08 23:01:56 +0100

    finds the wave crests

--- basilisk/stokes-ns-mask/read_vtu_python.py
+++ basilisk/stokes-ns-mask/read_vtu_python.py
@@ -3,12 +3,22 @@ import matplotlib.pyplot as plt
 from numpy import mean
 import numpy as np
 import scipy
-   
-filename = "vtu/ascii/TIME-000*_000017/TIME-000*_000017_0.vtu"
+import glob
 
-mesh = read(filename) 
+filenames = glob.glob('vtu/ascii*/*.vtu')
+
+"""
+# read all the files 
+vtu = {}
+i = 0
+for filename in filenames:
+    mesh = read(filename)
+    vtu[i] = mesh
+    i += 1
+"""
 
 
+mesh = read(filenames[17])
 cell_points = mesh.cells[0].data    
 points = mesh.points
 
@@ -32,6 +42,20 @@ center_U = np.stack([U,V], axis=1)
 
 plt.quiver(X[mask], Y[mask], U[mask], V[mask])
 plt.show()
+
+
+### FIND THE WAVE CRESTS ###
+#X0 that runs from min to max x value for finding wave crests
+X0 = np.linspace(min(X), max(X), 10000)
+#Y0 at a level under the wave troughs 
+Y0 = -max(Y)*1.2*np.ones_like(X0)
+interpolation_points = np.stack([X0, Y0], axis=1)
+interpolated_U = scipy.interpolate.griddata(center_points, center_U, interpolation_points) 
+#find the local maxima in ux to locate the wave crests
+x_ind = scipy.signal.argrelextrema(interpolated_U[:,0], np.greater) 
+print(x_ind)
+
+
 
 Y0 = np.linspace(-0.53, 0.0093, 1000)
 X0 = -0.0755*np.ones_like(Y0)

4f6a6bc7e2991dfbf4597247b5c731e0df5626fc
martingim <martingimse@gmail.com>
2024-02-23 10:47:23 +0100

    changed the lowpass parameter
    The Jensen 2001 images needed a higher lowpass parameter

--- matlab/ex2/find_crest.m
+++ matlab/ex2/find_crest.m
@@ -6,8 +6,8 @@ V(isnan(V))=0;
 v_abs = abs(V);
 v_mean = mean(v_abs, 1);
 
-%perform lowpass to smooth the data
-v_mean_lowpass = lowpass(v_mean, 0.001);
+%perform lowpass to smooth the data 0.001
+v_mean_lowpass = lowpass(v_mean, 0.2);
 %remove the leftmost and rightmost part of the data
 v_mean_lowpass(1:floor(size(v_mean_lowpass,2)*0.25)) = NaN;
 v_mean_lowpass(floor(size(v_mean_lowpass,2)*0.75):end) = NaN;

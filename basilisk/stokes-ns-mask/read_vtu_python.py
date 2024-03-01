e07a78bf66d58bcb290d8061d8ea5522356610ec
martingim <martingimse@gmail.com>
2024-03-01 13:38:37 +0100

    read all files
    now reads all the vtu files
    interpolates over a regular grid
    saves the data to a matlab file

--- basilisk/stokes-ns-mask/read_vtu_python.py
+++ basilisk/stokes-ns-mask/read_vtu_python.py
@@ -5,46 +5,51 @@ import numpy as np
 import scipy
 import glob
 
-filenames = glob.glob('vtu/ascii*/TIME*/*.vtu')
+matlab_filename  = "basilisk_velocity_profile.mat"
+matlab_dict  = {}
+
+
+nx = 100 #number of points to save in x direction in the matlab matrix
+ny = 100 # y dir
+filenames = glob.glob('vtu/ascii/*.vtu')
 filenames.sort()
-"""
-# read all the files 
-vtu = {}
+ 
 i = 0
 for filename in filenames:
+    print('reading file :', filename)
     mesh = read(filename)
-    vtu[i] = mesh
+    cell_points = mesh.cells[0].data
+    points = mesh.points
+    #calculate the cell centers
+    X = mean(points[cell_points],axis=1)[:,0]
+    Y = mean(points[cell_points],axis=1)[:,1]
+    center_points = np.stack([X, Y], axis=1)
+
+    
+    # get the data for the center of the cells
+    cellData = mesh.cell_data
+    f = cellData['f'][0]
+    mask = f>0.5
+
+    Ux = cellData['u.x']
+    U = Ux[0][:,0]
+    V = Ux[0][:,1]
+    center_U = np.stack([U,V], axis=1)
+    x_interpolation_points = np.linspace(min(X), max(X), nx)
+    y_interpolation_points = np.linspace(min(Y), max(Y), ny)
+    x,y= np.meshgrid(x_interpolation_points, y_interpolation_points)
+    interpolation_points = np.stack([x,y], axis=2)
+    interpolated_U = scipy.interpolate.griddata(center_points, center_U, interpolation_points)
+    interpolated_f = scipy.interpolate.griddata(center_points, f, interpolation_points)
+    res = {}
+    res['U'] = interpolated_U
+    res['X'] = interpolation_points
+    res['mask'] = interpolated_f>0.5 
+    matlab_dict[f"timestep_{i}"] = res
     i += 1
+scipy.io.savemat(matlab_filename, matlab_dict)
+
 """
-
-
-mesh = read(filenames[0])
-cell_points = mesh.cells[0].data    
-points = mesh.points
-
-def get_cell_centers():
-    return mean(points[cell_points],axis=1)
-    
-
-    
-
-cellData = mesh.cell_data
-f = cellData['f'][0]
-mask = f>0.5
-
-Ux = cellData['u.x']
-U = Ux[0][:,0]
-V = Ux[0][:,1]
-
-X = get_cell_centers()[:,0]
-Y = get_cell_centers()[:,1]
-center_points = np.stack([X, Y], axis=1)
-center_U = np.stack([U,V], axis=1)
-
-plt.quiver(X[mask], Y[mask], U[mask], V[mask])
-plt.show()
-
-
 ### FIND THE WAVE CRESTS ###
 #multiply the y coordinates with the water fraction to find the highest point of the wave
 Y_F = Y*f
@@ -60,11 +65,12 @@ plt.plot(interpolated_U[:,0], interpolation_points[:,1])
 plt.xlabel("u[m/s]")
 plt.ylabel("y")
 plt.show()
+i += 1
 
-
-matlab_dict  = {}
 matlab_dict['X'] = interpolation_points
 matlab_dict['U'] = interpolated_U
-matlab_filename  = "basilisk_velocity_profile.mat"
 
-scipy.io.savemat(matlab_filename, matlab_dict)
+
+plt.quiver(X[mask], Y[mask], U[mask], V[mask])
+plt.show()
+"""

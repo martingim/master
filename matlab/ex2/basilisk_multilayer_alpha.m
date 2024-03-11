23099ab871ba3917a084ade7fc88c5ddc10bc287
martingim <martingimse@gmail.com>
2024-03-11 17:46:13 +0100

    changes to results file structure and file naming for easier plotting of more results

--- matlab/ex2/basilisk_multilayer_alpha.m
+++ matlab/ex2/basilisk_multilayer_alpha.m
@@ -1,16 +1,17 @@
-function [] = basilisk_multilayer_alpha(a, k, omega, h)
+function [] = basilisk_multilayer_alpha(a, k, omega, h, nx, nl)
 
-[times,res] = load_multilayer_2d('basilisk_results/velocities.csv');
-times = [times(1) times(end)]; %only plot first and last timestep
+[times,res] = load_multilayer_2d(nx, nl);
+timesteps = [1 numel(times)]; %only plot first and last timestep
+timesteps = 1:numel(times);%plot all timesteps
 g = 9.81;
 N = 1000;
-for i = 1:numel(times)
+for i = timesteps
     t = times(i); 
     X = squeeze(res(i,1,:,:));
-    Y = squeeze(res(i,2,:,:));
-    U = squeeze(res(i,3,:,:));
-    V = squeeze(res(i,4,:,:));
-    eta=squeeze(res(i,5,:,:));
+    Y = squeeze(res(i,3,:,:));
+    U = squeeze(res(i,4,:,:));
+    V = squeeze(res(i,5,:,:));
+    eta=squeeze(res(i,6,:,:));
     alpha = omega/(a*g*k)*(U.^2+V.^2).^.5;
     % calculate the mean
     [ymax, ind] = max(max(Y));
@@ -21,6 +22,9 @@ for i = 1:numel(times)
         mean_alpha(N-height+1:end, i) = interp1(Y(:,i),alpha(:,i),y(N-height+1:end));
     end
     mean_alpha = mean(mean_alpha, 2, 'omitnan');
-    plot(mean_alpha, y/h,'--', 'DisplayName', sprintf("multilayer at time t=%.1f", t))
+    plot(mean_alpha, y/h,'--', 'DisplayName', sprintf("nx=%d, nl=%d, t=%.1f", nx, nl, t))
+
+    
 end
 
+end

0c44560004f06a9089814f1e6af1ad72f10210c6
martingim <martingimse@gmail.com>
2024-03-22 19:26:25 +0100

    changed the format so that it works but I need to find eta

--- basilisk/piston/make_piston_data.py
+++ basilisk/piston/make_piston_data.py
@@ -4,17 +4,23 @@ filename  = "waveinput.dat"
 
 ntimesteps = 24000
 
-f = 1 #frequency of the piston in hz
-a = 0.1 #amplitude of the piston motion
-T = 10
+f = 0.5 #frequency of the piston in hz
+a = 0.01 #amplitude of the piston motion
+T = 20
 t = np.linspace(0,T,ntimesteps)
-x = a*np.cos(t*2*np.pi*f)
-v = a*np.sin(t*2*np.pi*f)
+x = a*np.sin(t*2*np.pi*f)
+v = a*np.pi*2*f*np.sin(t*2*np.pi*f)
 
 with open(filename, 'w') as f:
-    f.write("@v213")
-    f.write(f"[piston wavemaker properties]\n")
-    f.write(f"ntimesteps {ntimesteps}\n")
+    f.write("@v213\n")
+    f.write("""[wave type]
+wavemaker
+[general input data]
+depth  0.6
+swl  0.
+[pistonwavemaker wave properties]
+#alpha_z, alpha_u
+0. 0.\n""")
+    f.write(f"#ntimestep\n{ntimesteps}\n")
     for i in range(ntimesteps):
-        f.write(f"{t[i]:.5f} {x[i]:.7f} {v[i]:.7f}\n")
-
+        f.write(f"{t[i]:.4f} 0.0 {v[i]:.7f} {x[i]:.7f}\n")

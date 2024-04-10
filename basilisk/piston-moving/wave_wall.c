3131d2e5c8c7556bfa36d006b49580690133d03d
martingim <martingimse@gmail.com>
2024-04-10 13:19:45 +0200

    adaptive mesh keeps interface at highest refinement
    updated constants
    changed the formula for the piston speed to use omega instead of f
    moved the piston closer to the left boundary
    changed the length of the tank to be power of two for easier initialization

--- basilisk/piston-moving/wave_wall.c
+++ basilisk/piston-moving/wave_wall.c
@@ -1,8 +1,9 @@
 /**
- * 
+ * based on http://basilisk.fr/sandbox/Antoonvh/wave_wall.c
  */
 #include <sys/stat.h>
 #include "utils.h"
+#include "adapt_wavelet_leave_interface__.h"
 
 #include "navier-stokes/centered.h"
 #include "two-phase.h"
@@ -10,31 +11,23 @@
 
 #include "output_vtu_foreach.h"
 
-int LEVEL = 10;
-int MAXLEVEL = 12;
-double femax = 0.001;
+int LEVEL = 4;
+int MAXLEVEL = 10;
+double femax = 0.1;
 double uemax = 0.01;
 double Tend = 10.;
-double piston_amplitude = 0.1;
+double piston_amplitude = 0.02;
 char save_location[] = "./";
-double frequency = 1.4244;
+double omega = 8.95;
 
-/**
-No-slip side walls and set and the moving piston parameters are chosen
-adhoc.
-*/
-//u.t[right] = dirichlet (0.);
-//u.t[left] = dirichlet (0.);
+double xp = 0.1; //the starting position of the piston (at rest leftmost position)
+double h = .9;   //Max. height
+double hb = 0.0; //Height above bottom floor
 
-double U_max = 1.5, //Maximum velocity
-  tau = 0.25,       //Time scale for moving
-  xp = 1.,         //Start location
-  w = 0.25,         //Width
-  h = .9,           //Max. height
-  hb = 0.0;           //Height above bottom floor
-
+#define w .1 //width of the piston
 #define _h 0.6//water depth
-#define U_X (piston_amplitude*sin(t*frequency*3.14*2))
+//#define XP (-piston_amplitude*cos(t*omega))
+#define U_X (piston_amplitude*omega*sin(t*omega))  //piston velocity from pos -cos(t*omega)
 #define PISTON (w - (x > xp) - (y > h) - (y < hb))
 scalar pstn[];
 
@@ -42,20 +35,19 @@ scalar pstn[];
 Furthermore, domain size and fluid properties are also chosen adhoc.
  */
 
-
 void mask_domain(){
-  mask(y > 2.5 ? top : none);
+  mask(y > 1 ? top : none);
 }
 
 
 int main() {
   mkdir("./vtu",0755);
-  L0 = 10;
-  mu1 = 0.01;
-  mu2 = 0.04;
+  L0 = 8;
   f.sigma = 0.01;
-  rho1 = 1000.;
-  rho2 = 1.;
+  rho1 = 1025;
+  rho2 = 1.225;
+  mu1 = 8.9e-4; 
+  mu2 = 17.4e-6;
   N = 1 << LEVEL;
   run();
 }
@@ -68,46 +60,48 @@ is targeted towards the piston and not towards the actual wave - basin
 wall collision.
  */
 event init (i = 0) {
-  
+  init_grid (1 << (MAXLEVEL));  
   pstn.prolongation = pstn.refine = fraction_refine;
-  fraction (f, _h - y); //Water depth $h = 2$
- 
-  while (adapt_wavelet((scalar *){pstn, f, u}, (double[]){femax, femax, uemax, uemax} , MAXLEVEL).nf){
-    fraction (f, _h - y);
-    adapt_wavelet((scalar *){pstn, f, u}, (double[]){femax, femax, uemax, uemax} , MAXLEVEL);
-  }
-   mask_domain();
+  fraction (f, _h - y); //Water depth _h
+  //while(adapt_wavelet_leave_interface({u.x, u.y},{pstn,f},(double[]){uemax,uemax,femax,femax}, MAXLEVEL, LEVEL,1).nf);{
+  //  fraction(f, _h-y);
+  //  fraction(pstn, PISTON);
+  //}
+  mask_domain();
 }
 /**
-The force of gravity is included with $g = 10$
+The force of gravity is included with $g = 9.81$
  */
 event acceleration (i++) {
   face vector av = a;
   foreach_face(y)
-    av.y[] = -10;
+    av.y[] = -9.81;
 }
 /**
 The moving piston is implemented via Stephane's trick. Note that this
 piston is leaky.
  */
-event piston (i++; t < 10) {
-  xp += dt*U_X;
+event piston (i++; t < Tend) {
+  xp += dt*U_X; //update the piston position based on the piston speed
+  //xp = XP;      //update the piston position based on the given formula
   fraction (pstn, PISTON);
   foreach() {
     u.y[] *= (1 - pstn[]);
     u.x[] = u.x[]*(1 - pstn[]) + pstn[]*U_X;
   }
+  printf("\nU_X:%f\n", U_X);
 }
 /**
 The grid is adapted to accuracy represent the piston fraction field,
 the water fraction fild and the velocity components. 
  */
 
-event adapt (i++)
-  adapt_wavelet ((scalar *){pstn, f, u}, (double[]){femax, femax, uemax, uemax} , MAXLEVEL);
-
+event adapt (i++){
+  printf("\nadapting mesh\n");
+  adapt_wavelet_leave_interface({u.x, u.y},{pstn,f},(double[]){uemax, uemax, femax, 1.0}, MAXLEVEL, LEVEL,0);
+}
 //save unordered mesh
-event vtu(t+=.1;t<10){
+event vtu(t+=.01;t<Tend){
   char filename[40];
   sprintf(filename, "%svtu/TIME-%06g", save_location, (t*100));
   output_vtu((scalar *) {f,p,pstn}, (vector *) {u}, filename);

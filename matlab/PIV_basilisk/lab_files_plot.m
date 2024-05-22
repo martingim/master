5ad3f77b488ac403a3f737b94e475004821b86b6
martingim <martingimse@gmail.com>
2024-05-22 11:25:13 +0200

    simpler to change which files to plot

--- matlab/PIV_basilisk/lab_files_plot.m
+++ matlab/PIV_basilisk/lab_files_plot.m
@@ -1,21 +1,24 @@
 clear all
 close all
+%% plot the piston movements
+lab_folder_number = 1;
+lab_folder = sprintf("Lab_results/24_04_12/%d/" , lab_folder_number);
 figure;
 hold on;
-load Lab_results/24_04_12/1/fil1.dat
+load(append(lab_folder, "fil1.dat"));
 fil = fil1;
 t = 0.008:0.01:size(fil1, 1)/100;
 
 plot(t, fil-fil(1,1))
-load Lab_results/24_04_12/1/fil2.dat
+load(append(lab_folder, "fil2.dat"));
 fil = fil2;
 plot(t, fil-fil(1,1))
 
-load Lab_results/24_04_12/1/fil3.dat
+load(append(lab_folder, "fil3.dat"));
 fil = fil3;
 plot(t, fil-fil(1,1))
 
-load Lab_results/24_04_12/1/padle_ut.dat
+load(append(lab_folder, "padle_ut.dat"));
 plot(t, (padle_ut-padle_ut(1)))
 
 legend('fil1', 'fil2', 'fil3', 'padle ut')
@@ -23,15 +26,27 @@ legend('fil1', 'fil2', 'fil3', 'padle ut')
 hs = 0:1/1.425:60;
 xs = zeros(size(hs));
 plot(hs,xs, 'X');
+title("plot of the paddle files");
 
-%% plot 308 amplitude results
+%% plot with basilisk results
 close all
+
+%basilisk_folder ="~/2piston/"; lab_run_number = 4;
+basilisk_folder = "~/piston-moving/"; lab_run_number=2;
+
+%load the lab results
+lab_folder = "Lab_results/24_04_12/";
+filenames = ["f1425_a0_308.csv";
+            "f1425_a0_308_r2.csv";
+            "f1425_a0_308_r3.csv";
+            "f1425_a0_616_r1.csv";
+            "f1425_a0_154_r1.csv"];
+file = load(append(lab_folder, filenames(lab_run_number)));
+t = 0.008:0.008:size(file, 1)/125;
+%plot the lab results
 figure;
 hold on
-load Lab_results/24_04_12/f1425_a0_308.csv
-file = f1425_a0_308;
-t = 0.008:0.008:size(file, 1)/125;
-col = 7;
+
 for col=3:3
     max(file(:,col)) - min(file(:,col))/2;
     plot(t, file(:,col)-file(1,col))
@@ -46,40 +61,15 @@ legend('sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6')
 % plot(X_0_1(:,1), X_0_1(:,2), 'DisplayName','abacus basilisk')
 % 
 
-load basilisk_results/surface_probes_15.csv
-surface_probes = surface_probes_15;
+%load and plot the basilsk results
+surface_probes = load(append(basilisk_folder, "surface_probes.csv"));
 plot(surface_probes(:,1), surface_probes(:,2),'DisplayName','NS')
+title(sprintf("lab run number:%d", lab_run_number));
 
- 
+% X marks the period 
 % hs = 0:1/1.425:60;
 % xs = zeros(size(hs));
 % plot(hs,xs, 'X');
-%% plot 616 amplitude results
-close all
-figure;
-hold on
-load Lab_results/24_04_12/f1425_a0_616_r1.csv
-file = f1425_a0_616_r1;
-t = 0.008:0.008:size(file, 1)/125;
-for col=3:3
-    max(file(:,col)) - min(file(:,col))/2;
-    plot(t, file(:,col)-file(1,col))
-end
-
-legend('sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6')
-% 
-% load basilisk_results/X_0
-% plot(X_0(:,1), X_0(:,2), 'DisplayName','local basilisk')
-% 
-% load basilisk_results/X_0_1
-% plot(X_0_1(:,1), X_0_1(:,2), 'DisplayName','abacus basilisk')
-
-% 
-load basilisk_results/surface_probes_double_15.csv
-surface_probes_double = surface_probes_double_15;
-plot(surface_probes_double(:,1), surface_probes_double(:,2),'DisplayName','NS')
-title('double piston amplitude results')
-
 
 %%
 load Lab_results/24_04_12/4/padle_ut.dat

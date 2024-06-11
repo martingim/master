660c08cbc6658b29a7ca498b773a06b4af31c518
martingim <martingimse@gmail.com>
2024-06-11 12:29:07 +0200

    added plotting 3rd and 5th

--- matlab/PIV_basilisk/Stokes5th_alpha.m
+++ matlab/PIV_basilisk/Stokes5th_alpha.m
@@ -10,14 +10,16 @@ T = 2*pi/omega;
 Result = StokesDispSolver('h', h, 'H', H,  'T', T, 'mode', 1);
 
 
-z = linspace(-h,a, 100);
+z = linspace(-h,a, 1000);
 [phi, u,u1,u2,u3,~,~, w,w1,w2,w3,~,~] = StokesU(Result.k, Result.h, Result.a, 0, z);
 u_3 = u1+u2+u3;
 w_3 = w1+w2+w3; 
 alpha = omega/(a*g*Result.k)*(u.^2+w.^2).^.5;
 alpha_3 = omega/(a*g*Result.k)*(u_3.^2+w_3.^2).^.5;
 if plot_figure
-    plot(alpha, z/h,'.', 'DisplayName',"stokes 5th theoretical")
+    hold on
+    plot(alpha, z/h, 'DisplayName',"stokes 5th theoretical")
+    plot(alpha_3, z/h, 'DisplayName',"stokes 3rd theoretical")
 end
 k = Result.k;
 end

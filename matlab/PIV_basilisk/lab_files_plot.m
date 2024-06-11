5aab293297e2482216419e73c1d374e33432732b
martingim <martingimse@gmail.com>
2024-06-11 12:32:30 +0200

    added plotting multiple basilisk runs and sensors

--- matlab/PIV_basilisk/lab_files_plot.m
+++ matlab/PIV_basilisk/lab_files_plot.m
@@ -30,10 +30,29 @@ title("plot of the paddle files");
 
 %% plot with basilisk results
 close all
+basilisk_folders = [];
+titles = [];
+%half amplitude piston movement
+%basilisk_folder = "~/Documents/master/basilisk_results/a_154/LEVEL15_embed_reduced/"; lab_run_number=5;
+%basilisk_folder = "~/Documents/master/basilisk_results/a_154/LEVEL13_embed_reduced/"; lab_run_number=5;
+%basilisk_folder = "~/Documents/master/basilisk_results/a_154/LEVEL14/"; lab_run_number=5;
 
-%basilisk_folder ="~/2piston/"; lab_run_number = 4;
-basilisk_folder = "~/piston-moving/"; lab_run_number=2;
+%308 piston amplitude
+lab_run_number = 1;
+%basilisk_folder = "~/Documents/master/basilisk_results/a_308/LEVEL/"; lab_run_number=2;
+%normal amplitude piston movement
+basilisk_folders = [basilisk_folders "~/Documents/master/basilisk_results/a_308/LEVEL10/"]; titles = [titles "LEVEL 10, 2.4 cm cells"];
+basilisk_folders = [basilisk_folders "~/Documents/master/basilisk_results/a_308/LEVEL11/"]; titles = [titles "LEVEL 11, 1.2 cm cells"];
+basilisk_folders = [basilisk_folders "~/Documents/master/basilisk_results/a_308/LEVEL12/"]; titles = [titles "LEVEL 12, 0.6 cm cells"];
+basilisk_folders = [basilisk_folders "~/Documents/master/basilisk_results/a_308/LEVEL13/"]; titles = [titles "LEVEL 13, 0.3 cm cells"];
+basilisk_folders = [basilisk_folders "~/Documents/master/basilisk_results/a_308/LEVEL13_2/"]; titles = [titles "LEVEL 13_2"];
+basilisk_folders = [basilisk_folders "~/Documents/master/basilisk_results/a_308/LEVEL13_wavetank/"]; titles = [titles "LEVEL 13 not reduced"];
 
+basilisk_folder = "~/Documents/master/basilisk_results/a_308/LEVEL13_no_tension/piston-moving/"; lab_run_number=1;
+
+
+omega = 8.95; 
+T = (2*pi)/omega;
 %load the lab results
 lab_folder = "Lab_results/24_04_12/";
 filenames = ["f1425_a0_308.csv";
@@ -43,37 +62,92 @@ filenames = ["f1425_a0_308.csv";
             "f1425_a0_154_r1.csv"];
 file = load(append(lab_folder, filenames(lab_run_number)));
 t = 0.008:0.008:size(file, 1)/125;
-%plot the lab results
+probe_positions = [8 10 10.7 11.5];
+%plot the lab and basilisk results
+surface_probes = load(append(basilisk_folder, "surface_probes.csv"));
 figure;
-hold on
+tiledlayout(4,1);
 
-for col=3:3
+for sensor=1:4
+    nexttile;
+    hold on
+    
+    %plot the lab results
+    col = sensor+2;
     max(file(:,col)) - min(file(:,col))/2;
-    plot(t, file(:,col)-file(1,col))
+    plot(t, file(:,col)-file(1,col), 'DisplayName','surface probe')
+
+    %plot the basilisk results
+    plot(surface_probes(:,1), surface_probes(:,sensor+1),'DisplayName','basilisk')
+    title(sprintf("sensor %d, x=%.1f", sensor, probe_positions(sensor)))
+    legend()
+    xlabel('t[s]')
+    ylabel('surface eleveation[m]')
+    xlim([0 30])
+    %mark the period
+    %plot(0:T:60,0, 'x')
 end
 
-legend('sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5', 'sensor6')
-% 
-% load basilisk_results/X_0
-% plot(X_0(:,1), X_0(:,2), 'DisplayName','local basilisk')
-% 
-% load basilisk_results/X_0_1
-% plot(X_0_1(:,1), X_0_1(:,2), 'DisplayName','abacus basilisk')
-% 
+%% plot sensor 1 for different basilisk runs
+sensor = 1;
+figure;
+tiledlayout(6,1)
 
-%load and plot the basilsk results
-surface_probes = load(append(basilisk_folder, "surface_probes.csv"));
-plot(surface_probes(:,1), surface_probes(:,2),'DisplayName','NS')
-title(sprintf("lab run number:%d", lab_run_number));
+for i=1:size(basilisk_folders, 2)
+    basilisk_folder = basilisk_folders(i);
+    titl = titles(i);
+    nexttile;
+    hold on
 
-% X marks the period 
-% hs = 0:1/1.425:60;
-% xs = zeros(size(hs));
-% plot(hs,xs, 'X');
-
+    %plot the lab results
+    col = sensor+2;
+    max(file(:,col)) - min(file(:,col))/2;
+    plot(t, file(:,col)-file(1,col), 'DisplayName','surface probe')
+    
+    %plot the basilisk results
+    surface_probes = load(append(basilisk_folder, "surface_probes.csv"));
+    plot(surface_probes(:,1), surface_probes(:,sensor+1),'DisplayName','basilisk')
+    title(titl)
+    legend()
+    xlabel('t[s]')
+    ylabel('surface eleveation[m]')
+    xlim([0 30])
+    %mark the period
+    %plot(0:T:60,0, 'x')
+end
 %%
-load Lab_results/24_04_12/4/padle_ut.dat
-load Lab_results/24_04_12/4/fil3.dat
-%plot((padle_ut-padle_ut(1)))
+figure;
+hold on;
+for lab_run_number = 1:5
+    load(sprintf("Lab_results/24_04_12/%d/padle_ut.dat", lab_run_number))
+    plot(padle_ut-padle_ut(1), 'DisplayName',sprintf("run number:%d",lab_run_number))
+end
+legend()
+title("padle_ut")
 
-plot(fil3-fil3(1))
+figure;
+hold on;
+for lab_run_number = 1:5
+    load(sprintf("Lab_results/24_04_12/%d/fil1.dat", lab_run_number))
+    plot(fil1-fil1(1), 'DisplayName',sprintf("run number:%d",lab_run_number))
+end
+legend()
+title("fil1")
+
+figure;
+hold on;
+for lab_run_number = 1:5
+    load(sprintf("Lab_results/24_04_12/%d/fil2.dat", lab_run_number))
+    plot(fil2-fil2(1), 'DisplayName',sprintf("run number:%d",lab_run_number))
+end
+legend()
+title("fil2")
+
+figure;
+hold on;
+for lab_run_number = 1:5
+    load(sprintf("Lab_results/24_04_12/%d/fil3.dat", lab_run_number))
+    plot(fil3-fil3(1), 'DisplayName',sprintf("run number:%d",lab_run_number))
+end
+legend()
+title("fil3")

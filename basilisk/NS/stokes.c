660b58eb8d8e2b4df7d5b0cf205d568e0207cb6e
martingim <martingimse@gmail.com>
2024-12-03 10:13:02 +0100

    surface probe and command line args

--- basilisk/NS/stokes.c
+++ basilisk/NS/stokes.c
@@ -10,6 +10,7 @@ account using the "reduced gravity approach". */
 #include "navier-stokes/conserving.h"
 #include "reduced.h"
 #include "output_vtu_foreach.h"
+#include "heights.h"
 int vtucount = 0;
 
 /**
@@ -19,6 +20,13 @@ number. */
 double ak = 0.17;
 int LEVEL = 7;
 double Lx = 0.7903377744879982;
+
+vector h[]; //scalar field of the distance from the surface, using heights.h
+char results_folder[40]; //the location to save the results
+char vtu_folder[50]; //the locaton to save the vtu files
+
+int n_probes = 1;
+double probe_positions[] = {0.00001};
 /**
 The error on the components of the velocity field used for adaptive
 refinement. */
@@ -38,7 +46,7 @@ these values. *T0* is simulation duration */
 #define k_  7.95
 #define h_   0.6
 #define g_   9.81
-#define T0  20
+#define T0  10
 
 /**
 The program takes optional arguments which are the level of
@@ -46,10 +54,28 @@ refinement, steepness and Reynolds numbers. */
 
 int main (int argc, char * argv[])
 {
-  if (argc > 1)
-    LEVEL = atoi (argv[1]);
-  if (argc > 2)
-    ak = atof(argv[2]);
+  for(int j=0;j<argc;j++){
+    if (strcmp(argv[j], "-L") == 0) 
+    {                 
+      LEVEL = atoi(argv[j + 1]);
+    }
+  }
+  sprintf(results_folder, "results/LEVEL%d", LEVEL);
+  sprintf(vtu_folder, "%s/vtu", results_folder);
+
+
+  char remove_old_results[100];
+  sprintf(remove_old_results, "rm -r %s", results_folder);
+  if (system(remove_old_results)==0){
+    printf("removed old results in:%s\n", results_folder);
+  }
+
+  char make_results_folder[100];
+  sprintf(make_results_folder, "mkdir -p %s", vtu_folder);
+  if (system(make_results_folder)==0){
+    printf("made results folder:%s\n", results_folder);
+  }
+  
 
   L0 = Lx;   
   origin (-L0/2, -h_);
@@ -118,29 +144,29 @@ event init (i = 0)
   }
 }
 
-event profiles (t += T0/4.; t <= 2.5*T0) {
-  output_facets (f, stderr);
-  fprintf (stderr, "\n");
-}
+// event profiles (t += T0/4.; t <= 2.5*T0) {
+//   output_facets (f, stderr);
+//   fprintf (stderr, "\n");
+// }
 
-event logfile (i++)
-{
-  double ke = 0., gpe = 0.;
-  foreach (reduction(+:ke) reduction(+:gpe)) {
-    double norm2 = 0.;
-    foreach_dimension()
-      norm2 += sq(u.x[]);
-    ke += norm2*f[]*dv();
-    gpe += y*f[]*dv();
-  }
-  printf ("%g %g %g\n", t/(k_/sqrt(g_*k_)), rho1*ke/2., rho1*g_*gpe + 0.125);
-}
+// event logfile (i++)
+// {
+//   double ke = 0., gpe = 0.;
+//   foreach (reduction(+:ke) reduction(+:gpe)) {
+//     double norm2 = 0.;
+//     foreach_dimension()
+//       norm2 += sq(u.x[]);
+//     ke += norm2*f[]*dv();
+//     gpe += y*f[]*dv();
+//   }
+//   printf ("%g %g %g\n", t/(k_/sqrt(g_*k_)), rho1*ke/2., rho1*g_*gpe + 0.125);
+// }
 
 //save unordered mesh
 event vtu(t+=.1, last){
   printf("Saving vtu file\n");
   char filename[100];
-  sprintf(filename, "TIME-%05.0f", (t*100));
+  sprintf(filename, "%s/TIME-%05.0f", vtu_folder, (t*100));
   output_vtu((scalar *) {f,p}, (vector *) {u}, filename);
 }
 
@@ -173,3 +199,36 @@ event adapt (i++) {
   adapt_wavelet ({f,u}, (double[]){0.01,uemax,uemax,uemax}, LEVEL, 5);
 }
 #endif
+
+event surface_probes(t+=0.01, t<T0){
+  char filename[100];
+  sprintf(filename, "%s/surface_probes.csv", results_folder);
+  FILE *fp = fopen(filename, "a");
+    if (i==0){
+      fprintf(fp, "time");
+      for (int j = 0;j<n_probes;j++){
+        fprintf(fp, ", %f", probe_positions[j]);    
+      }
+      fprintf(fp, "\n");
+  }
+  fprintf(fp, "%f", t);
+  heights(f, h);
+  double min_height; //vertical distance from the center of the cell to the air water interface
+  double surface_elevation=0;
+  for (int probe_n=0;probe_n<n_probes;probe_n++){
+    min_height=1;
+    foreach(serial){
+      if((fabs(x-probe_positions[probe_n])<Delta/2.0+1e-9)&&(fabs(y)<0.1)){
+        if(h.y[] != nodata){
+          if (fabs(min_height)>fabs(height(h.y[])*Delta)){
+            min_height=height(h.y[])*Delta;
+            surface_elevation = y + height(h.y[])*Delta;
+          }
+        }
+      }
+    }
+    fprintf(fp, ", %f", surface_elevation);
+  }
+  fprintf(fp, "\n");
+  fclose(fp);
+}

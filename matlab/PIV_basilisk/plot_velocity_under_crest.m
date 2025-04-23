ada4680ddd81a209e06b242bdf045f56101215f6
martingim <martingimse@gmail.com>
2025-04-23 14:57:35 +0200

    Changed to standard deviation of zhao

--- matlab/PIV_basilisk/plot_velocity_under_crest.m
+++ matlab/PIV_basilisk/plot_velocity_under_crest.m
@@ -13,7 +13,7 @@ load params.mat params
 
 %load parameters for plotting analytical solutions
 water_depth = image_params('water_depth');
-water_depth = water_depth(run_number)
+water_depth = water_depth(run_number);
 
 p = params(run_number);
 a = p('a');
@@ -64,50 +64,58 @@ crest_mask = idx(:,crest_idx)&true;
 u_crest_scaled = 1/(a*omega)*u_crest;
 y_scaled = 1/water_depth*yw(crest_mask);
 
-%% create shaded area based on the standard deviation of the amplitude'
-yw_at_crest = yw(:,crest_idx);
 
+figure('Position', [1000, 818,1000,800]);
+fontsize(100, "points")
+hold on
+%% Zhao 5th order with uncertainty in a
+
+Resultmax = StokesDispSolver('h', 0.6, 'H', 2*(a+a_std), 'T', 1/1.425, 'mode', 1);
+Resultmin = StokesDispSolver('h', 0.6, 'H', 2*(a-a_std), 'T', 1/1.425, 'mode', 1);
+
+yw_at_crest = yw(:,crest_idx);
+yw_analytical = min(yw_at_crest):0.0001:a;
+
+
+% create shaded area based on the standard deviation of the amplitude'
 yw_min = min(yw_at_crest):0.0001:a-a_std;
 y_min = 1/water_depth*yw_min;
 
 yw_max = min(yw_at_crest):0.0001:a+a_std;
 y_max = 1/water_depth*yw_max;
 
-
-[~, u_max,~,~,~,~,~, ~,~,~,~,~,~] = FentonU(k, water_depth, a+a_std, 0, yw_max);
-[~, u_min,~,~,~,~,~, ~,~,~,~,~,~] = FentonU(k, water_depth, a-a_std, 0, yw_min);
+[~, u_max,~,~,~,~,~, ~,~,~,~,~,~] = StokesU(Resultmax.k, water_depth, a+a_std, 0, yw_max);
+[~, u_min,~,~,~,~,~, ~,~,~,~,~,~] = StokesU(Resultmin.k, water_depth, a-a_std, 0, yw_min);
 
 u_min = transpose(1/(a*omega)*u_min(:,1));
 u_max = transpose(1/(a*omega)*u_max(:,1));
 
-figure;
-hold on
 
-f = fill([u_min flip(u_max)], [y_min flip(y_max)], [0.8 0.8 0.8]);
+
+f = fill([u_min flip(u_max)], [y_min flip(y_max)], [0.8 0.8 0.8], 'DisplayName',"One standard deviation in the amplitude");
 f.EdgeAlpha = 0;
 % plot the analytical velocity
+Result = StokesDispSolver('h', 0.6, 'H', 2*a, 'T', 1/1.425, 'mode', 1);
+[~, u,~,~,~,~,~, ~,~,~,~,~,~] = StokesU(Result.k, 0.6, a, 0, yw_analytical);
+plot(1/(a*omega)*u, yw_analytical/water_depth, 'DisplayName', 'Zhao et. al 2022')
 
-
-yw_analytical = min(yw_at_crest):0.0001:a;
 [~, u,~,~,~,~,~, ~,~,~,~,~,~] = FentonU(k, water_depth, a, 0, yw_analytical);
 y_analytical_scaled = 1/water_depth*yw_analytical;
-plot(1/(a*omega)*u, y_analytical_scaled);
+plot(1/(a*omega)*u, y_analytical_scaled,'color', "red","DisplayName","Fenton's solution");
 
 
 %plot calculated velocity
-plot(abs(u_crest_scaled), y_scaled, 'x')
+plot(abs(u_crest_scaled), y_scaled, 'x','color', 'black', "DisplayName","PIV experiment", 'MarkerSize',10)
 
 
-legend('one standard deviation in the amplitude', 'analytical solution', 'experimental','Location','southeast')
-title(sprintf('horizontal velocity under the crest run:%d, wave pair:%d', run_number, pair_number))
+legend('Location','southeast')
+title(sprintf('Horizontal velocity under the crest'))
 
 xlabel('$\frac{v}{a\omega}$', 'interpreter', 'latex', 'FontSize', 20)
 ylabel('$\frac{y}{h}$', 'interpreter', 'latex', 'FontSize', 20, 'rotation', 0)
+fontsize(20, "points")
 %% plot Stokes 5th order
-Result = StokesDispSolver('h', 0.6, 'H', 2*a, 'T', 1/1.425, 'mode', 1);
-%FentonResult = FentonDispSolver('h', 0.6, 'H', 2*a, 'T', 1/1.425, 'mode', 1);
-[~, u,~,~,~,~,~, ~,~,~,~,~,~] = StokesU(Result.k, 0.6, a, 0, yw_analytical);
-plot(1/(a*omega)*u, yw_analytical/water_depth, 'DisplayName', 'Zhao')
+
     
 %plot(1/(a*omega)*FentonU(FentonResult.k, 0.6, a*2, 0, yw_analytical), yw_analytical/water_depth, 'DisplayName', 'Fenton')
 end

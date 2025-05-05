1de07712591acff8c0f407d96087a561f02a8375
martingim <martingimse@gmail.com>
2025-05-05 11:28:58 +0200

    changed the conversion parameters

--- matlab/PIV_basilisk/run1_compare_piston_files.m
+++ matlab/PIV_basilisk/run1_compare_piston_files.m
@@ -7,7 +7,7 @@ lab_surface_probes = ["f1425_a0_308.csv";
 run_number = 1;
 %% plot
 close all;
-
+    
 run_folder = sprintf("%s%d/" , lab_folder, run_number);
 load(append(run_folder, "tid.dat"))
 t = tid;
@@ -20,11 +20,11 @@ title("Piston position files run number 1, 12/04/2024");
 load(append(run_folder, "fil1.dat"));
 fil = fil1;
 %t = 0:0.01:(size(fil1, 1)-1)/100;
-plot(t, -(fil-fil(1,1))*0.044, 'DisplayName','fil1.dat*0.042')
+plot(t, -(fil-fil(1,1))*0.044, 'DisplayName','fil1.dat*0.044')
 
 load(append(run_folder, "fil2.dat"));
 fil = fil2;
-plot(t, (fil-fil(1,1))*0.0046, 'DisplayName','fil2.dat*0.0046')
+plot(t, (fil-fil(1,1))*0.0044, 'DisplayName','fil2.dat*0.0044')
 
 load(append(run_folder, "fil3.dat"));
 fil = fil3;
@@ -32,10 +32,10 @@ plot(t, (fil-fil(1,1))*0.01, 'k--','DisplayName','fil3.dat*0.01')
 
 load(append(run_folder, "padle_ut.dat"));
 
-plot(t(1:size(padle_ut, 1)), (padle_ut-padle_ut(1))*0.042, 'DisplayName','padle\_ut.dat*0.042')
+plot(t(1:size(padle_ut, 1)), (padle_ut-padle_ut(1))*0.044, 'DisplayName','padle\_ut.dat*0.044')
 xlim([0, 3])
 
-plot(t, 0.042*0.308*tanh(t).*sin(2*pi*1.425*(t)), 'DisplayName','0.042\cdot0.308\cdottanh(t)\cdotsin(2\pift)')
+plot(t, 0.044*0.308*tanh(t).*sin(2*pi*1.425*(t)), 'DisplayName','0.044\cdot0.308\cdottanh(t)\cdotsin(2\pift)')
 legend('Location','southeast')
 xlabel('time [s]')
 ylabel('piston position [m]')
@@ -44,9 +44,10 @@ ylabel('piston position [m]')
 
 nexttile;
 hold on;
-plot(t(1:size(padle_ut, 1)), (padle_ut-padle_ut(1))*0.042, 'DisplayName','padle\_ut.dat')
-plot(t, 0.042*0.308*tanh(t).*sin(2*pi*1.425*(t)), '--','DisplayName','tanh(t)*sin(2\pift)')
+plot(t(1:size(padle_ut, 1)), (padle_ut-padle_ut(1))*0.044, 'DisplayName','padle\_ut.dat')
+plot(t, 0.044*0.308*tanh(t).*sin(2*pi*1.425*(t)), '--','DisplayName','0.044*0.308*tanh(t)*sin(2\pift)')
 plot(t(1:4:end)-0.03, (fil(1:4:end)-fil(1,1))*0.01,'xk', 'DisplayName','fil3.dat shifted by 0.03s', 'MarkerSize',10)
+plot(t-0.03, (fil2-fil2(1))*0.0044, 'DisplayName',"fil2*0.0044")
 legend('Location','southeast')
 xlabel('time [s]')
 ylabel('piston position [m]')

4384f80cd442356e10520b3b7cccece206dd45c4
martingim <martingimse@gmail.com>
2025-05-21 07:37:51 +0200

    changed the run paramters

--- basilisk/2d_piston/boundary-piston/bash_run_script.sh
+++ basilisk/2d_piston/boundary-piston/bash_run_script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-r="11"
+r="1"
 for n_threads in "1" "2" "3" "4" "5" "6" "7" "8"
 do
-    for L in "11"
+    for L in "10"
     do
-        echo "./piston -L ${L} -r ${r} |& tee -a LEVEL${L}_r${r}_log.txt"
-        ./piston -L "${L}" -r "${r}" --nthreads "${n_threads}" |& tee  "LEVEL${L}_r${r}_n${n_threads}_log.txt"
+        #echo "./piston -L ${L} -r ${r} |& tee -a LEVEL${L}_r${r}_log.txt"
+        ./piston -L "${L}" -r "${r}" --nthreads "${n_threads}" |& tee  "LEVEL${L}_r${r}_n${n_threads}_log_5s.txt"
+        #./piston -L "${L}" -r "${r}" |& tee  "run11/LEVEL${L}_r${r}_log.txt"
     done
 done

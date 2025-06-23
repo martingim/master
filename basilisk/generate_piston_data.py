00478449ae2d9099e326d5fa42dbb817a2bd3ad8
martingim <martingimse@gmail.com>
2025-06-23 20:42:04 +0200

    changed to larger offset and symmetric about the two middle pistons

--- basilisk/generate_piston_data.py
+++ basilisk/generate_piston_data.py
@@ -19,9 +19,9 @@ file_samplerate = 100
 end_time = 20
 
 f = 1.425
-wave_time = 1/f*5 #how many amplitudes to generate
-piston_amplitude = 0.0125
-piston_phase_offset = 0.1 # how much the pistons lag behind each other
+wave_time = 1/f*10 #how many amplitudes to generate
+piston_amplitude = 0.025
+piston_phase_offset = 0.2 # how much the pistons lag behind each other
 
 
 generated_position = np.zeros((end_time*file_samplerate,number_of_pistons))
@@ -31,14 +31,10 @@ offset = 0
 
 for i in range(number_of_pistons):
     generated_position[int(offset*file_samplerate):len(t)+int(offset*file_samplerate),i] = piston_amplitude*np.sin((t)*f*2*np.pi)*np.tanh(t)*np.tanh(wave_time-t)
-    offset += piston_phase_offset
-
+    if i<6:
+        offset += piston_phase_offset
+    if i>6:
+        offset -= piston_phase_offset
 
 np.savetxt(speed_file, np.gradient(generated_position, axis=0)*file_samplerate, fmt='%.8f')
 np.savetxt(position_file, generated_position, fmt='%.8f')
-
-
-
-#plt.plot(fft_smoothed)
-#plt.plot(generated_position)
-#plt.show()
